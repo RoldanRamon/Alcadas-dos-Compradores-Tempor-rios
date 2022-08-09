ecd640b88534f3b7fb1fa27c8805f30f470cceb1
Ramon Roldan de Lara <ramon.roldan@grupomarista.org.br>
2022-08-09 17:47:25 -0300

    rodando atualizacao

--- desenvolvimento do relatorio.R
+++ desenvolvimento do relatorio.R
@@ -6,12 +6,28 @@ library(janitor)
 library(tidyr)
 
 #Carrega Bases
-filiais <- read_excel(path = 'filiais.xlsx',sheet = 'filiais') %>% clean_names()
-usuarios <- read_excel(path = 'filiais.xlsx',sheet = 'usuarios') %>% clean_names()
+filiais <- read_excel(path = 'NOME DOS GRUPOS SOS TELECOM_EMPRESA_FILIAL.xlsx') %>% clean_names()
+usuarios <- read_excel(path = 'filiais.xlsx',sheet = 'lista') %>% clean_names()
+
+#filiais
+filiais_1 <- filiais %>% filter(nomedogrupo == "OC -SOS TELECOM – SUPRIMENTOS 1 R$ 0,01 A R$ 1.000,0")
+filiais_2 <- filiais %>% filter(nomedogrupo == "OC -SOS TELECOM – SUPRIMENTOS 2 R$ 1.000,01 A R$ 30.000,00")
+filiais_3 <- filiais %>% filter(nomedogrupo == "OC -SOS TELECOM – SUPRIMENTOS 3 R$ 30.000,01 A R$ 2.000.000,00")
+filiais_4 <- filiais %>% filter(nomedogrupo == "OC -SOS TELECOM – SUPRIMENTOS 4 ACIMA DE R$ 2.000.000,01")
+
+#usuarios
+usuarios_1 <- usuarios %>% filter(grupo == "OC -SOS TELECOM – SUPRIMENTOS 1 R$ 0,01 A R$ 1.000,0")
+usuarios_2 <- usuarios %>% filter(grupo == "OC -SOS TELECOM – SUPRIMENTOS 2 R$ 1.000,01 A R$ 30.000,00")
+usuarios_3 <- usuarios %>% filter(grupo == "OC -SOS TELECOM – SUPRIMENTOS 3 R$ 30.000,01 A R$ 2.000.000,00")
+usuarios_4 <- usuarios %>% filter(grupo == "OC -SOS TELECOM – SUPRIMENTOS 4 ACIMA DE R$ 2.000.000,01")
+
 
 #Tratamento da Base
-base <- filiais %>%
-  mutate(teste = map(.x = handle,.f = ~ bind_cols(usuarios))) %>% unnest(cols = teste)
+base <- bind_rows(filiais_1 %>% mutate(teste = map(.x = handle,.f = ~ bind_cols(usuarios_1))) %>% unnest(cols = teste),
+                  filiais_2 %>% mutate(teste = map(.x = handle,.f = ~ bind_cols(usuarios_2))) %>% unnest(cols = teste),
+                  filiais_3 %>% mutate(teste = map(.x = handle,.f = ~ bind_cols(usuarios_3))) %>% unnest(cols = teste),
+                  filiais_4 %>% mutate(teste = map(.x = handle,.f = ~ bind_cols(usuarios_4))) %>% unnest(cols = teste)
+                  )
 
 #Exportar base
 writexl::write_xlsx(base,'arquivo_final.xlsx')
